92114e82ea53632704e55c7b91db32e1d69bb287
Sue Anne <sueanneteo@gmail.com>
2014-01-04 17:49:23 +0100

    added admin users and permissions on delete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		# sign in user and redirect to the user show page
 	else 
 		flash.now[:error] = "Invalid Email address / Password combination"
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 end
 
 	def destroy
-		sign_out
-		redirect_to root_path
+		sign_out 
+		redirect_to root_url
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admmin_user, only: :destroy
+
+
+  def index
+    @users = User.all
+  end
+
+
   def new
   	@user = User.new
   end
@@ -10,6 +20,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      sign_in @user 
   		flash[:success] = "Welcome to Reviews Singapore!"
   		redirect_to @user 
   	else
@@ -17,6 +28,24 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user 
+    else
+      #direct to edit path again
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
   
 
   private
@@ -24,5 +53,21 @@ class UsersController < ApplicationController
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+    redirect_to signin_path, notice: "Please sign in." 
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
   
 end
